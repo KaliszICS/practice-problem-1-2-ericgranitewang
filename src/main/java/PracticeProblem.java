6719879810baff308b4af6ecd1bdd08f5f3059dc
ericgranitewang <166552552+ericgranitewang@users.noreply.github.com>
2026-03-03 15:00:10 +0000

    commit real

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,28 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static boolean validIndex(int[] a, int b) {
+		try {
+			return a[b] >= Integer.MIN_VALUE;
+		} catch (IndexOutOfBoundsException e) {
+			return false;
+		}
+	}
+
+	public static int divide (int a, int b) {
+		try {
+			return a/b;
+		} catch (ArithmeticException e) {
+			return 0;
+		}
+	}
+
+	public static int safeConvertStringtoInt(String a) {
+		try {
+			return Integer.parseInt(a);
+		} catch (NumberFormatException e) {
+			return 0;
+		}
+	}
 
 }
